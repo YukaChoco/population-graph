f663a334b4ff159e5c82a6d141466ad3808e037d
YukaChoco <sugar.ore.yk@gmail.com>
2023-09-30 23:58:17 +0900

    関数を呼ぶ位置をコンポーネントからページに修正

--- src/hooks/usePrefectures.ts
+++ src/hooks/usePrefectures.ts
@@ -5,18 +5,18 @@ import axios from 'axios';
 export default function usePrefectures() {
   const [prefectures, setPrefectures] = useState<Prefecture[] | null>(null);
 
+  const getPrefectureData = async () => {
+    try {
+      const response = await axios.get<GetPrefecture>('/api/prefecture');
+      const { result } = response.data;
+      setPrefectures(result);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get<GetPrefecture>('/api/prefecture');
-        const { result } = response.data;
-        setPrefectures(result);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-    fetchData();
-    console.log('useEffect Run');
+    getPrefectureData();
   }, []);
 
   return { prefectures };
