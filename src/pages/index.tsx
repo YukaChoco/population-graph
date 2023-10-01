1323bdaaa7b29636eb1625a48861e68eb2452761
YukaChoco <sugar.ore.yk@gmail.com>
2023-10-02 05:47:25 +0900

    loadingのブール値表示削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,7 +31,6 @@ export default function Home() {
             handleChange={handlePrefectureSelected}
             setLoading={setLoading}
           />
-          {`${loading}`}
         </>
       );
     }
