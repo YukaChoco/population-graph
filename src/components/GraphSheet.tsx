7a3c011d2753bbeab2cab154323c2ba57e3eb926
YukaChoco <sugar.ore.yk@gmail.com>
2023-10-02 06:31:29 +0900

    PopulationTypeSelecterの作成

--- src/components/GraphSheet.tsx
+++ src/components/GraphSheet.tsx
@@ -10,7 +10,7 @@ interface Props {
     prefectures: Prefecture[];
   };
   populationTypeSelecter: {
-    handlePopulationTypeSelecter: () => void;
+    handlePopulationTypeSelecter: (value: string) => void;
   };
 }
 
--- src/components/PopulationTypeSelecter.tsx
+++ src/components/PopulationTypeSelecter.tsx
@@ -1,7 +1,22 @@
+import { POPULATION_TYPE } from '@/const';
+import styles from '@/styles/PopulationTypeSelecter.module.css';
+
 interface Props {
-  handlePopulationTypeSelecter: () => void;
+  handlePopulationTypeSelecter: (value: string) => void;
 }
 
 export default function PopulationTypeSelecter(props: Props) {
-  return <div>PopulationTypeSelecter</div>;
+  const { handlePopulationTypeSelecter } = props;
+  return (
+    <div className={styles.container}>
+      <select
+        className={styles.selecter}
+        onChange={(event) => handlePopulationTypeSelecter(event.target.value)}
+      >
+        {POPULATION_TYPE.map((type, index) => (
+          <option key={index}>{type}</option>
+        ))}
+      </select>
+    </div>
+  );
 }
