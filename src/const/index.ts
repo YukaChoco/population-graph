d437febd86e1d80884359682ffb762505fb5d386
YukaChoco <sugar.ore.yk@gmail.com>
2023-10-02 03:17:40 +0900

    都道府県によるグルーピング

--- src/const/index.ts
+++ src/const/index.ts
@@ -3,3 +3,14 @@ export const API_HEADERS = {
     'X-API-KEY': process.env.API_KEY,
   },
 };
+
+export const PREFECTURES_GROUP = [
+  { groupName: '北海道地方', prefCode: [1] },
+  { groupName: '東北地方', prefCode: [2, 3, 4, 5, 6, 7] },
+  { groupName: '関東地方', prefCode: [8, 9, 10, 11, 12, 13, 14] },
+  { groupName: '中部地方', prefCode: [15, 16, 17, 18, 19, 20, 21, 22, 23] },
+  { groupName: '近畿地方', prefCode: [24, 25, 26, 27, 28, 29, 30] },
+  { groupName: '中国地方', prefCode: [31, 32, 33, 34, 35] },
+  { groupName: '四国地方', prefCode: [36, 37, 38, 39] },
+  { groupName: '九州地方', prefCode: [40, 41, 42, 43, 44, 45, 46, 47] },
+];
