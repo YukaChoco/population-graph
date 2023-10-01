7432754e284f4b726eabe58e6be719413d7636fc
YukaChoco <sugar.ore.yk@gmail.com>
2023-10-01 22:52:38 +0900

    新しいPrefecture(PopulationData)型でpopulationをreturn

--- src/hooks/usePopulation.ts
+++ src/hooks/usePopulation.ts
@@ -3,12 +3,21 @@ import axios from 'axios';
 import { GetPopulation, GetPrefecturePopulation } from '@/types/Population';
 
 export default function usePopulation() {
+  const getDisaplayPopulationData = (result: GetPrefecturePopulation) => {
+    const displayDatas = result.data.map((populationDatas) => ({
+      label: populationDatas.label,
+      data: populationDatas.data.map((populationData) => populationData.value),
+    }));
+    return displayDatas;
+  };
+
   const getPopulationData = async (prefCode: number) => {
     try {
       const response = await axios.get<GetPopulation>(
         `/api/population?prefCode=${prefCode}`,
       );
-      const { data } = response.data.result;
+      const { result } = response.data;
+      const data = getDisaplayPopulationData(result);
       return data;
     } catch (error) {
       console.error('Error fetching data:', error);
