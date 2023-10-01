2306839de50a9f5e492bc3d240c4e8491226fdc8
YukaChoco <sugar.ore.yk@gmail.com>
2023-10-01 17:02:30 +0900

    都道府県取得時に都道府県の配列を作成

--- src/hooks/usePrefectures.ts
+++ src/hooks/usePrefectures.ts
@@ -1,15 +1,30 @@
 import { useState, useEffect } from 'react';
-import Prefecture, { GetPrefecture } from '@/types/Prefecture';
+import Prefecture, {
+  GetPrefecture,
+  PrefectureCodeName,
+} from '@/types/Prefecture';
 import axios from 'axios';
 
 export default function usePrefectures() {
   const [prefectures, setPrefectures] = useState<Prefecture[] | null>(null);
 
+  const setPrefectureArray = (prefectures: PrefectureCodeName[]) => {
+    const prefectureArray: Prefecture[] = prefectures.map(
+      (prefecture, index) => ({
+        ...prefecture,
+        selected:
+          index === 0 || index === prefectures.length - 1 ? true : false,
+        data: [],
+      }),
+    );
+    return prefectureArray;
+  };
+
   const getPrefectureData = async () => {
     try {
       const response = await axios.get<GetPrefecture>('/api/prefecture');
       const { result } = response.data;
-      setPrefectures(result);
+      setPrefectures(setPrefectureArray(result));
     } catch (error) {
       console.error('Error fetching data:', error);
     }
