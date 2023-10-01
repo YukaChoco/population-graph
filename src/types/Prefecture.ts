c192371f758d8f21b3570e2ee8669b63c3c5aa8f
YukaChoco <sugar.ore.yk@gmail.com>
2023-10-01 16:22:50 +0900

    Prefectureの型を修正

--- src/types/Prefecture.ts
+++ src/types/Prefecture.ts
@@ -1,9 +1,16 @@
-export default interface Prefecture {
+import { PopulationData } from './Population';
+
+export default interface Prefecture extends PrefectureCodeName {
+  selected: boolean;
+  data: PopulationData[];
+}
+
+export interface PrefectureCodeName {
   prefCode: number;
   prefName: string;
 }
 
 export interface GetPrefecture {
   message: string;
-  result: Prefecture[];
+  result: PrefectureCodeName[];
 }
