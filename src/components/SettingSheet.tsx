3170d94a8b0f23e7fa91e8e49c8dbdd869d18c62
YukaChoco <sugar.ore.yk@gmail.com>
2023-10-01 22:04:00 +0900

    SettingSheetやそのコンポーネントにpropsを追加

--- src/components/SettingSheet.tsx
+++ src/components/SettingSheet.tsx
@@ -1,13 +1,28 @@
 import styles from '@/styles/SettingSheet.module.css';
 import CheckBox from './atom/CheckBox';
+import Prefecture from '@/types/Prefecture';
 
-export default function SettingSheet() {
+interface Props {
+  prefectures: Prefecture[];
+  handlePrefectureSelected: () => void;
+}
+
+export default function SettingSheet(props: Props) {
   return (
     <div className={styles.sheet}>
       都道府県を選択してください
-      <CheckBox />
-      <CheckBox />
-      <CheckBox />
+      <CheckBox
+        prefecture={props.prefectures[0]}
+        handleChange={props.handlePrefectureSelected}
+      />
+      <CheckBox
+        prefecture={props.prefectures[1]}
+        handleChange={props.handlePrefectureSelected}
+      />
+      <CheckBox
+        prefecture={props.prefectures[2]}
+        handleChange={props.handlePrefectureSelected}
+      />
     </div>
   );
 }
