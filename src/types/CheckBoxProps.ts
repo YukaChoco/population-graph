5b94d6964c3a174317c6fccb9b16af056cf4bf29
YukaChoco <sugar.ore.yk@gmail.com>
2023-10-08 21:40:42 +0900

    SetLoadingPropsの作成、使用

--- src/types/CheckBoxProps.ts
+++ src/types/CheckBoxProps.ts
@@ -1,7 +1,8 @@
 import type Prefecture from '@/types/Prefecture';
 import type HandlePrefectureSelectedProps from '@/types/HandlePrefectureSelectedProps';
+import type SetLoadingProps from './SetLoadingProps';
 
 export default interface CheckBoxProps extends Pick<Prefecture, 'prefName' | 'prefCode' | 'selected'> {
   handleChange: HandlePrefectureSelectedProps['handlePrefectureSelected'];
-  setLoading: (selected: boolean) => void;
+  setLoading: SetLoadingProps['setLoading'];
 }
--- src/types/SettingSheetProps.ts
+++ src/types/SettingSheetProps.ts
@@ -1,9 +1,10 @@
 import type Prefecture from '@/types/Prefecture';
 import type HandlePrefectureSelectedProps from '@/types/HandlePrefectureSelectedProps';
+import type SetLoadingProps from './SetLoadingProps';
 
 export default interface SettingSheetProps {
   prefectures: Prefecture[];
   handleChange: HandlePrefectureSelectedProps['handlePrefectureSelected'];
-  setLoading: (selected: boolean) => void;
+  setLoading: SetLoadingProps['setLoading'];
 }
 
