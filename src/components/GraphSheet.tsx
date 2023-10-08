603989903a9d36c04a24982a53b181a950e93231
YukaChoco <sugar.ore.yk@gmail.com>
2023-10-08 21:22:41 +0900

    GraphSheetの初期値設定

--- src/components/GraphSheet.tsx
+++ src/components/GraphSheet.tsx
@@ -1,11 +1,17 @@
 import styles from '@/styles/GraphSheet.module.css';
 import PopulationGraph from './PopulationGraph';
 import PopulationTypeSelecter from './PopulationTypeSelecter';
-import GraphSheetProps from '@/types/GraphSheetProps';
-import Prefecture from '@/types/Prefecture';
+import type GraphSheetProps from '@/types/GraphSheetProps';
+import type Prefecture from '@/types/Prefecture';
 
-export default function GraphSheet(props: GraphSheetProps) {
-  const { populationType, labels, prefectures, getPopulationWithType, handlePopulationTypeSelecter } = props;
+export default function GraphSheet({
+  populationType = '',
+  labels = [],
+  prefectures = [],
+  getPopulationWithType = (prefecture: Prefecture, type: string) => [],
+  handlePopulationTypeSelecter = (value: string) => { },
+}: GraphSheetProps) {
+
   return (
     <div className={styles.sheet}>
       <PopulationTypeSelecter
