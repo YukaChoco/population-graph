464fd8617803b283d790b4cac772cb6267060cf9
YukaChoco <sugar.ore.yk@gmail.com>
2023-10-08 21:16:41 +0900

    未使用の型削除

--- src/types/CheckBoxProps.ts
+++ src/types/CheckBoxProps.ts
@@ -1,6 +1,5 @@
 import type Prefecture from '@/types/Prefecture';
 import type HandlePrefectureSelected from '@/types/HandlePrefectureSelected';
-import type { Dispatch, SetStateAction } from 'react';
 
 export default interface CheckBoxProps extends Pick<Prefecture, 'prefName' | 'prefCode' | 'selected'> {
   handleChange: HandlePrefectureSelected['handlePrefectureSelected'];
