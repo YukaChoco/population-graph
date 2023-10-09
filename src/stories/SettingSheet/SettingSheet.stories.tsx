cc1c704933660902a02a5598e394426e6ed43d36
YukaChoco <sugar.ore.yk@gmail.com>
2023-10-10 06:07:21 +0900

    SettingSheetのタイポ修正

--- src/stories/SettingSheet/SettingSheet.stories.tsx
+++ src/stories/SettingSheet/SettingSheet.stories.tsx
@@ -14,6 +14,6 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = { args: args };
 export const SomeSelected: Story = { args: customArgs.someSelected };
-export const Notselected: Story = { args: customArgs.notSelected };
+export const NotSelected: Story = { args: customArgs.notSelected };
 export const FewPrefectures: Story = { args: customArgs.fewPrefectures };
 export const NoPrefectures: Story = { args: customArgs.noPrefectures };
