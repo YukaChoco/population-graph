6c325b68cebacb196a9f126937b5d2e51c67b380
YukaChoco <sugar.ore.yk@gmail.com>
2023-10-10 07:49:02 +0900

    POPULATION_TYPEを統一

--- src/stories/const/populationTypes.ts
+++ src/stories/const/populationTypes.ts
@@ -1,12 +1,7 @@
-export const POPULATION_TYPES = [
-  "総人口",
-  "年少人口",
-  "生産年齢人口",
-  "老年人口",
-];
+import { POPULATION_TYPE } from "@/const";
 
 export const ANOTHER_POPULATION_TYPES = [
-  ...POPULATION_TYPES,
+  ...POPULATION_TYPE,
   "幼少人口",
   "その他の人口",
 ];
